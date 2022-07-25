a934da77e1cebc0629e176959750b41cf9acfac1
unknown <andresfelipehc@ufps.edu.co>
2022-07-25 16:56:30 -0500

    implementando validacion de campos

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,7 +1,13 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
+import javax.validation.Valid;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +22,17 @@ public class FormController {
 		return "form";
 	}
 	
-	@PostMapping("/form")
-	public String procesar(Usuario usuario, Model model) {
+	@PostMapping("/form")// La anotación valid para validar los parametros, y el result para verificar si hay algun error
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+		
+		if(result.hasErrors()) {
+			Map<String, String> errores = new HashMap<>();
+			result.getFieldErrors().forEach(err ->{
+				errores.put(err.getField(), "El campo".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+			});
+			model.addAttribute("error", errores);
+			return "form";
+		}
 		
 		model.addAttribute("usuario", usuario);
 		
