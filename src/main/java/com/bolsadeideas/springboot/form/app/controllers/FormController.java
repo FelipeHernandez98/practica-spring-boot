76504acbf606da08c1dcaf3a676f2ea2556bc72c
unknown <andresfelipehc@ufps.edu.co>
2022-07-26 10:37:52 -0500

    modificando las validaciones propias de thymeleaf

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -2,15 +2,13 @@ package com.bolsadeideas.springboot.form.app.controllers;
 
 import javax.validation.Valid;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
@@ -19,6 +17,8 @@ public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model  model) {
+		Usuario usuario = new Usuario();
+		model.addAttribute("usuario", usuario);
 		return "form";
 	}
 	
@@ -26,11 +26,7 @@ public class FormController {
 	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
 		
 		if(result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err ->{
-				errores.put(err.getField(), "El campo".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-			});
-			model.addAttribute("error", errores);
+			
 			return "form";
 		}
 		
